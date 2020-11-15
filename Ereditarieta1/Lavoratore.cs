bdd8cc33204c4e8d41d8de9ab42af04b75cb3e6e
michelangelo truffarelli <michelangelo.truffarelli@outlook.com>
2020-11-15 16:49:51 +0100

    Add project files.

---
+++ Ereditarieta1/Lavoratore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ereditarieta1
+{
+    internal class Lavoratore : Persona
+    {
+        internal double Stipendio { get; set; }
+
+        public Lavoratore(double s)
+        {
+            Nome = base.Nome;
+            Cognome = base.Cognome;
+            Stipendio = s;
+            Altezza = base.Altezza;
+            DataNascita = base.DataNascita;
+        }
+    }
+}
